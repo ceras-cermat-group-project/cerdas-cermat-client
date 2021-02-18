080df9eca4191a427c90a02ed51dfcbf61e13545
ezekielhutagalung <yehezkiel.life@gmail.com>
2021-02-19 00:47:53 +0700

    clientUpdate3

---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container d-flex justify content-center">
+  <form @submit.prevent="login">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Username</label>
+    <input type="text" class="form-control"  v-model="Username" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Login',
+  data () {
+    return {
+      Username: ''
+    }
+  },
+  components: {
+  },
+  methods: {
+    login () {
+      localStorage.setItem('Username', this.Username)
+      this.$router.push('/')
+    }
+  }
+}
+</script>
