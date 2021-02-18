d026f05e4905f7a31e0456e959bf506a46fff3e5
ezekielhutagalung <yehezkiel.life@gmail.com>
2021-02-19 04:28:25 +0700

    clientUpdate4

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <!-- <testing/> -->
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -9,8 +10,13 @@
 </template>
 
 <script>
+// import testing from './components/form'
 export default {
+  components: {
+    // testing
+  }
 }
 </script>
 <style>
+
 </style>
