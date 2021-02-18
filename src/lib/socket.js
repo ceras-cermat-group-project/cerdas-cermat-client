3438e0dba12d0a80af1943b354eae8385249ba2d
jefDimar <jefdimar@gmail.com>
2021-02-18 21:03:12 +0700

    done connecting client to server

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,9 +1,9 @@
-// import Vue from 'vue'
-// import VueSocketIO from 'vue-socket-io'
+import Vue from 'vue'
+import VueSocketIO from 'vue-socket.io'
 
-// const socket = new VueSocketIO({
-//   debug: true,
-//   connection: 'http://localhost:3000'
-// })
+const socket = new VueSocketIO({
+  debug: true,
+  connection: 'http://localhost:3000'
+})
 
-// Vue.use(socket)
+Vue.use(socket)
