272d148136a350d6f9043565872b3dd341a5a988
muhammadsuryo31 <muhammadsuryo31@gmail.com>
2021-02-19 13:09:35 +0700

    patch client

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,24 @@
 <template>
 <div class="bg">
+  <button type="button" class="btn btn-danger" @click="fetchQuiz">Start</button>
 <div class="container">
-<h1>{{quizzes}}</h1>
-<form>
+  <h1 style="color: green">{{score}}</h1>
+
+<form @submit.prevent="UserAnswer">
   <label>
-    <p class="label-txt"></p>
-    <input type="text" class="input">
-    <div class="line-box">
-      <div class="line"></div>
-    </div>
-  </label>
-  <label>
-    <p class="label-txt">Jawaban</p>
-    <input type="text" class="input">
+    <p class="label-txt" style="color: greeen">Jawaban</p>
+    <input type="text" class="input" v-model="answer">
     <div class="line-box">
       <div class="line"></div>
     </div>
   </label>
   <button type="submit">submit</button>
 </form>
-<div v-for="item in quizzes" :key="item.id"  class="col-md-4">
+<div class="col-md-4">
          <div class="card mb-4 text-white bg-dark">
-            <img class="card-img-top" src='' alt="Card image cap">
+            <img class="card-img-top" :src="getImage" alt="Card image cap">
             <div class="card-body">
-               <h5 class="card-title">Card title</h5>
-               <p class="card-text">{{item.question}}</p>
-               <a href="http://www.jquery2dotnet.com/" class="btn btn-outline-light btn-sm">Go somewhere</a>
+               <h5 class="card-title">pertanyaan {{quiz.id}}</h5>
             </div>
          </div>
       </div>
@@ -43,25 +36,61 @@
 <script>
 export default {
   name: 'Home',
+  data () {
+    return {
+      answer: ''
+    }
+  },
   sockets: {
     connect: function () {
       console.log('<<<<<< connected')
     },
     testKeClient (data) {
       console.log('dari server', data)
+    },
+    Quiz (data) {
+      this.$store.dispatch('fetchQuiz', data)
+    },
+    nextQuiz (payload) {
+      this.$store.dispatch('fetchQuiz', payload)
+    },
+    scoring (score) {
+      this.$store.dispatch('scoring', score)
     }
   },
   methods: {
+    UserAnswer () {
+      const payload = {
+        answer: this.answer,
+        quizId: this.$store.state.quiz.id,
+        UserId: this.$socket.id
+      }
+      // console.log(payload)
+      this.$socket.emit('inputUser', payload)
+      this.answer = ''
+    },
     testingConnect () {
+      // let inputuser =''
+      // let quizid = this.$store.state.quiz.quizId
+      // quizid++
       this.$socket.emit('testing', 'input, quizid')
+    },
+    fetchQuiz () {
+      const quizId = 1
+      this.$socket.emit('fetchQuiz', quizId)
+      // quizId++
     }
-  },
-  created () {
-    this.$store.dispatch('fetchQuiz')
+
   },
   computed: {
-    quizzes () {
-      return this.$store.state.quizzes
+    quiz () {
+      return this.$store.state.quiz
+    },
+    score () {
+      return this.$store.state.score
+    },
+    getImage () {
+      return this.quiz.question
     }
   }
 }
